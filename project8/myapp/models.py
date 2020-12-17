113eac58ec3180847bd2dd948a47a3070788a6c3
jerev7 <verdet.jeremie@gmail.com>
2020-12-17 13:44:22 +0100

    emptied and change tables in db

--- project8/myapp/models.py
+++ project8/myapp/models.py
@@ -3,17 +3,7 @@ from django.db import models
 # Create your models here.
 ### test with variables before using db ###
 
-CATEGORIES = []
 
-PRODUCTS = [
-	{'name': 'nutella', 'category': 'sucrerie'},
-	{'name': 'dragibus', 'category': 'sucrerie'},
-	{'name': 'ketchup', 'category': 'condiment'},
-	{'name': 'chips', 'category': 'aperitif'}	
-]
-for product in PRODUCTS:
-	if product['category'] not in CATEGORIES:
-		CATEGORIES.append(product['category']) 
 
 
 class Category(models.Model):
@@ -28,10 +18,8 @@ class Category(models.Model):
 
 class Products(models.Model):
 	name = models.CharField(max_length=200)
-	palm_oil = models.BooleanField(default=False)
-	gluten = models.BooleanField(default=False)
-	stores = models.CharField(max_length=200)
-	url = models.CharField(max_length=200)
+	nutriscore = models.IntegerField()
+	image_url = models.URLField(max_length=200)
 	categories = models.ManyToManyField(Category, related_name='product', blank=True)
 
 	class Meta:
