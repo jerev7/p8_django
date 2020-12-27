2e5ba1bde96b33b83ef11bdea02a81e18a932d4e
jerev7 <verdet.jeremie@gmail.com>
2020-12-27 17:39:57 +0100

    modified detail's url

--- project8/myapp/urls.py
+++ project8/myapp/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
 	# url(r'^$', views.listing, name="listing"),
-	url(r'^(?P<product_id>[0-6]+)/$', views.detail, name="detail"),
+	url(r'^(?P<product_id>[0-9]+)/$', views.detail, name="detail"),
 	url(r'^search/$', views.search, name="search")
 ]
