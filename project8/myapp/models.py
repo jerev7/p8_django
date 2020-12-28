8c4b3a04eb13933216778a460e62a32ae5c4ba67
jerev7 <verdet.jeremie@gmail.com>
2020-12-28 16:47:39 +0100

    changed the whole products table to add some attributes

--- project8/myapp/models.py
+++ project8/myapp/models.py
@@ -1,10 +1,6 @@
 from django.db import models
 
 # Create your models here.
-### test with variables before using db ###
-
-
-
 
 class Category(models.Model):
 	name = models.CharField(max_length=200)
@@ -20,6 +16,15 @@ class Products(models.Model):
 	name = models.CharField(max_length=200)
 	nutriscore = models.IntegerField()
 	image_url = models.URLField(max_length=200)
+	url_offacts = models.URLField(max_length=200)
+	energy_value = models.CharField(max_length=200)
+	energy_unit = models.CharField(max_length=200)
+	sugars_100g = models.CharField(max_length=200)
+	fat_100g = models.CharField(max_length=200)
+	saturated_fat_100g = models.CharField(max_length=200)
+	proteins = models.CharField(max_length=200)
+	nutriscore_letter_url = models.URLField(max_length=200)
+	nutriscore_complete_url = models.URLField(max_length=200)
 	categories = models.ManyToManyField(Category, related_name='product', blank=True)
 
 	class Meta:
