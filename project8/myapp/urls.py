446f82704a25677594202d5a9e417e3c6cf0c654
jerev7 <verdet.jeremie@gmail.com>
2021-01-02 15:08:59 +0100

    added the link to product_detail, and fixed the url

--- project8/myapp/urls.py
+++ project8/myapp/urls.py
@@ -5,5 +5,7 @@ from . import views
 urlpatterns = [
 	# url(r'^$', views.listing, name="listing"),
 	url(r'^(?P<product_id>[0-9]+)/$', views.results, name="results"),
-	url(r'^search/$', views.search, name="search")
+	url(r'^search/$', views.search, name="search"),
+	url(r'^product_detail/(?P<product_id>[0-9]+)/$', views.product_detail, name="product_detail"),
+
 ]
