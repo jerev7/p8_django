42cde2c8fbf3e40cfb6825874cd604f254fb749d
jerev7 <verdet.jeremie@gmail.com>
2021-01-08 16:57:33 +0100

    changed index nav bar with new logos etc

--- project8/myapp/static/myapp/js/scripts.js
+++ project8/myapp/static/myapp/js/scripts.js
@@ -61,3 +61,20 @@
   });
 
 })(jQuery); // End of use strict
+
+
+//////////// Personal adds /////////////////////////////////
+
+
+
+$(document).ready(function() {
+        // Navigation color change 
+        $(window).scroll(function() {
+        var nav_img = $('#carotte');
+        if ($(document).scrollTop() < 90) {
+            nav_img.attr("src","{% static 'myapp/assets/img/carotte_blanc.png' %}");
+        } else {
+            nav_img.attr("src","{% static 'myapp/assets/img/carotte_noir.png' %}");
+        }
+        });
+    });
