d868740498ec59cae0d283cae61f3579d2f27e65
jerev7 <verdet.jeremie@gmail.com>
2021-01-22 16:07:49 +0100

    fixed some display settings and add javascript function to update url in next pages of a search

--- project8/myapp/static/myapp/js/scripts.js
+++ project8/myapp/static/myapp/js/scripts.js
@@ -64,3 +64,9 @@
 
 
 //////////// Personal adds /////////////////////////////////
+
+function addUrlParameter(name, value) {
+  var searchParams = new URLSearchParams(window.location.search)
+  searchParams.set(name, value)
+  window.location.search = searchParams.toString()
+}
