5caa67317ee4990e4372e6a30d1b3d40e895479b
jerev7 <verdet.jeremie@gmail.com>
2021-05-19 14:51:40 +0200

    finished tests for registration

--- project8/registration/tests.py
+++ project8/registration/tests.py
@@ -11,13 +11,19 @@ from django.contrib.auth.models import User
 
 # Create an account
 class AccountPageTest(TestCase):
-    def test_account_creation(self):
-        new_form = RegisterForm()
-        # new_form.username = "Bob"
-        # new_form.email = "bobby@gmail.com"
-        # new_form.password1 = "mypass13"
-        # new_form.password2 = "mypass13"
-        response = self.client.post(reverse('register'), {'form': new_form})
+    def test_account_creation(self):        
+        response = self.client.post(reverse('register'), data={"username":"Bob", "email":"bob@mail.com", "password1":"monpassword23", "password2":"monpassword23"})
+        self.assertEqual(response.status_code, 302)
+        new_user = User.objects.get(username="Bob")
+        self.assertEqual(new_user.username, "Bob")
+        # self.assertEqual(response.status_code, 302)
+
+    def test_logged_out(self):
+        response = self.client.get(reverse('logged_out'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_registration_ok(self):
+        response = self.client.get(reverse('registration_ok'))
         self.assertEqual(response.status_code, 200)
 
 # Login
