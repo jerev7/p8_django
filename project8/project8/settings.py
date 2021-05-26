deb3b72f8f8cea2c2972726d3fad7780231298d4
jerev7 <verdet.jeremie@gmail.com>
2021-05-26 15:36:40 +0200

    changed allowed host in settings.py

--- project8/project8/settings.py
+++ project8/project8/settings.py
@@ -30,7 +30,7 @@ if os.environ.get('ENV') == "PRODUCTION":
 else:
     DEBUG = True
 
-ALLOWED_HOSTS = ["project8.herokuapp.com"]
+ALLOWED_HOSTS = ["p8django.herokuapp.com"]
 
 
 # Application definition
