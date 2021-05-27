d843063a18515986006bf1701c41ac3242fc24e8
jerev7 <verdet.jeremie@gmail.com>
2021-05-27 13:54:10 +0200

    wsgi

--- project8/project8/wsgi.py
+++ project8/project8/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project8.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project8.project8.settings')
 
 application = get_wsgi_application()
