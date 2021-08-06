96d0de15d3f360db9f89c9734a5e5e1f49984c91
jerev7 <verdet.jeremie@gmail.com>
2021-08-06 15:24:49 +0200

    first modif following flake8

--- project8/myapp/admin.py
+++ project8/myapp/admin.py
@@ -5,14 +5,20 @@ from .models import Category, Products, Product_saved
 # Register your models here.
 class ProductSavedAdmin(admin.ModelAdmin):
     pass
+
+
 admin.site.register(Product_saved, ProductSavedAdmin)
 
 
 class ProductsAdmin(admin.ModelAdmin):
     pass
+
+
 admin.site.register(Products, ProductsAdmin)
 
 
 class CategoryAdmin(admin.ModelAdmin):
     pass
+
+
 admin.site.register(Category, CategoryAdmin)
--- project8/myapp/tests.py
+++ project8/myapp/tests.py
@@ -2,14 +2,10 @@ from django.test import TestCase
 from django.urls import reverse
 from .models import Category, Products, Product_saved
 from django.contrib.auth.models import User
-from django.contrib.auth import authenticate
 from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import selenium.webdriver.support.ui as ui
-import time
-
-
 
 
 # Create your tests here.
@@ -17,41 +13,44 @@ import time
 testproduct = ""
 testproduct2 = ""
 
+
 def setting_up_db():
     testcategory = Category.objects.create(name="testcategory")
     testproduct = Products.objects.create(name="testproduct",
-                                     nutriscore=4,
-                                     image_url="/",
-                                     url_offacts="/",
-                                     energy_value=24.2,
-                                     energy_unit="kcal",
-                                     sugars_100g=12.1,
-                                     fat_100g=2,
-                                     saturated_fat_100g=1.2,
-                                     proteins=4,
-                                     nutriscore_complete_url="myapp/assets/img/nutriscore/nutricomplet_e.png",
-                                     nutriscore_letter_url="myapp/assets/img/nutriscore/nutrilettre_e.png")
+                                          nutriscore=4,
+                                          image_url="/",
+                                          url_offacts="/",
+                                          energy_value=24.2,
+                                          energy_unit="kcal",
+                                          sugars_100g=12.1,
+                                          fat_100g=2,
+                                          saturated_fat_100g=1.2,
+                                          proteins=4,
+                                          nutriscore_complete_url="myapp/assets/img/nutriscore/nutricomplet_e.png",
+                                          nutriscore_letter_url="myapp/assets/img/nutriscore/nutrilettre_e.png")
     testproduct2 = Products.objects.create(name="testproduct2",
-                                     nutriscore=3,
-                                     image_url="/",
-                                     url_offacts="/",
-                                     energy_value=24.2,
-                                     energy_unit="kcal",
-                                     sugars_100g=12.1,
-                                     fat_100g=2,
-                                     saturated_fat_100g=1.2,
-                                     proteins=4,
-                                     nutriscore_complete_url="myapp/assets/img/nutriscore/nutricomplet_d.png",
-                                     nutriscore_letter_url="myapp/assets/img/nutriscore/nutrilettre_d.png")
+                                           nutriscore=3,
+                                           image_url="/",
+                                           url_offacts="/",
+                                           energy_value=24.2,
+                                           energy_unit="kcal",
+                                           sugars_100g=12.1,
+                                           fat_100g=2,
+                                           saturated_fat_100g=1.2,
+                                           proteins=4,
+                                           nutriscore_complete_url="myapp/assets/img/nutriscore/nutricomplet_d.png",
+                                           nutriscore_letter_url="myapp/assets/img/nutriscore/nutrilettre_d.png")
     testproduct.categories.add(testcategory)
     testproduct2.categories.add(testcategory)
     user = User.objects.create(username="Bob", email="bob@gmail.com", password="bobby")
+
 
 # Index page
 class IndexPageTestCase(TestCase):
     def test_index_page(self):
         response = self.client.get(reverse('index'))
         self.assertEqual(response.status_code, 200)
+
 
 # Legal notices
 class LegalPageTestCase(TestCase):
@@ -62,7 +61,7 @@ class LegalPageTestCase(TestCase):
 
 # Search page
 class SearchPageTestCase(TestCase):
-    def setUp(self): # create data before tests start
+    def setUp(self):  # create data before tests start
         setting_up_db()
 
     def test_search_page_product_found(self):
@@ -73,9 +72,10 @@ class SearchPageTestCase(TestCase):
         response = self.client.get(reverse('search'), {'query': 'huhuhuhu'})
         self.assertEqual(response.status_code, 404)
 
+
 # Result page
 class ResultPageTestCase(TestCase):
-    def setUp(self): # create data before tests start
+    def setUp(self):  # create data before tests start
         setting_up_db()
 
     def test_result_page_product_found(self):
@@ -87,7 +87,7 @@ class ResultPageTestCase(TestCase):
 
 # Product detail
 class DetailPageTestCase(TestCase):
-    def setUp(self): # create data before tests start
+    def setUp(self):  # create data before tests start
         setting_up_db()
 
     def test_detail_page_product_found(self):
@@ -96,9 +96,10 @@ class DetailPageTestCase(TestCase):
         response = self.client.get(reverse('product_detail', args=(product_id,)))
         self.assertEqual(response.status_code, 200)
 
+
 # Save and delete a product
 class SaveProductPageTestCase(TestCase):
-    def setUp(self): # create data before tests start
+    def setUp(self):  # create data before tests start
         setting_up_db()
 
     def test_saveanddelete_product_page(self):
@@ -118,7 +119,7 @@ class SaveProductPageTestCase(TestCase):
         self.assertEqual(products_number_after, (products_number_before + 1))
 
         # Deleting new product
-        new_product_saved = Product_saved.objects.get(product_selected=product_to_save_selected, 
+        new_product_saved = Product_saved.objects.get(product_selected=product_to_save_selected,
                                                       substitution_product=product_to_save_substitution,
                                                       user=User.objects.get(username='Bob'))
         response2 = self.client.get(reverse('delete_product', args=(new_product_saved.id,)))
@@ -126,7 +127,8 @@ class SaveProductPageTestCase(TestCase):
         products_number_finally = len(Product_saved.objects.all())
         self.assertEqual(products_number_finally, products_number_before)
 
-###################### Selenium tests #############################
+
+# ------------------- Selenium tests ------------------------#
 class PlayerFormTest(LiveServerTestCase):
 
     def setUp(self):
@@ -143,23 +145,19 @@ class PlayerFormTest(LiveServerTestCase):
     #     """
     #     self.driver.quit()
     def test_search_page(self):
-        
-        #find the elements you need to submit form
+        # find the elements you need to submit form
         search_test = "nutella"
         form = self.driver.find_element_by_id('searchForm')
         form.send_keys(search_test)
         form.send_keys(Keys.ENTER)
         self.driver.implicitly_wait(100)
         ui.WebDriverWait(self.driver, 3000)
-        # url = self.driver.current_url
-        # self.assertEqual(url, "https://p8django.herokuapp.com/myapp/search/?query=nutella")
 
-        #testing search : nutella
+        # testing search : nutella
         product_searched = self.driver.find_element_by_id('product_searched').text
         self.assertEqual(product_searched, "Produit recherché : nutella")
         url = self.driver.current_url
         self.assertEqual(url, "https://p8django.herokuapp.com/myapp/search/?query=nutella")
-
 
         # selecting the first product
         self.driver.find_element_by_partial_link_text("Nutella").click()
@@ -170,9 +168,3 @@ class PlayerFormTest(LiveServerTestCase):
         self.driver.find_element_by_partial_link_text("Pâte").click()
         url = self.driver.current_url
         self.assertEqual(url, "https://p8django.herokuapp.com/myapp/product_detail/3/")
-
-        #submit form
-        # submit.send_keys(Keys.RETURN)
-
-        #check result; page source looks at entire html document
-        # assert 'Lebron James' in selenium.page_source
