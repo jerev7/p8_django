68cf8da5e098120ce975785c088b233b79403bf3
jerev7 <verdet.jeremie@gmail.com>
2021-08-10 16:06:12 +0200

    wrap regex in urls.py

--- project8/myapp/urls.py
+++ project8/myapp/urls.py
@@ -9,7 +9,8 @@ urlpatterns = [
         views.product_detail,
         name="product_detail"),
     url(r'^user_products/$', views.user_products, name="user_products"),
-    url(r'^save_product/(?P<product_selected_id>[0-9]+)/(?P<substitution_id>[0-9]+)/$',
+    url((r'^save_product/(?P<product_selected_id>[0-9]+)'
+         r'/(?P<substitution_id>[0-9]+)/$'),
         views.save_product, name="save_product"),
     url(r'^delete_product/(?P<product_id>[0-9]+)/$',
         views.delete_product,
